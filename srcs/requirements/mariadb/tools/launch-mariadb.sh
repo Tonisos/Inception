e6523c7282368db20e40bc799303244c2d0f1c55
42MacAntoine <amontalb@student.42lyon.fr>
2023-11-22 09:47:11 +0100

    patch mariadb

--- srcs/requirements/mariadb/tools/launch-mariadb.sh
+++ srcs/requirements/mariadb/tools/launch-mariadb.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
+# Terminate the script if an error occured
+set -e
+
 service mysql start;
+
+# Safe install of MySQL
+if [ ! -d "/var/lib/mysql/$WP_TITLE" ]
+
+then
+
+mysql_secure_installation << EOF
+
+n
+y
+y
+y
+y
+EOF
+
+# Generate the database and the user with privilege for WordPress
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${WP_TITLE}\`;"
 
@@ -11,6 +30,9 @@ mysql -e "GRANT ALL PRIVILEGES ON \`${WP_TITLE}\`.* TO \`${WP_USER_LOGIN}\`@'%' 
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 mysql -e "FLUSH PRIVILEGES;"
+
+fi
+
 
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
