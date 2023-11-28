2b0dfac93efe9abe03131ffcf6a1bb8d5a1c97c9
42MacAntoine <amontalb@student.42lyon.fr>
2023-11-28 13:01:47 +0100

    update wordpress

--- srcs/requirements/mariadb/tools/launch-mariadb.sh
+++ srcs/requirements/mariadb/tools/launch-mariadb.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+WP_URL=amontalb.42.fr
+WP_TITLE=wordpress
+WP_DB_HOST=mariadb
+SQL_ROOT_PASSWORD=root12345
+
+WP_ADMIN_LOGIN=boss
+WP_ADMIN_PASSWORD=boss12345
+WP_ADMIN_EMAIL=boss@42.fr
+
+WP_USER_LOGIN=amontalb
+WP_USER_PASSWORD=12345
+WP_USER_EMAIL=amontalb@42.fr
+
 # Terminate the script if an error occured
 set -e
 
@@ -19,17 +32,18 @@ y
 y
 EOF
 
-Generate the database and the user with privilege for WordPress
+# Generate the database and the user with privilege for WordPress
 
-mysql -e "CREATE DATABASE IF NOT EXISTS ${WP_TITLE};"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${WP_TITLE}\';"
 
 mysql -e "CREATE USER IF NOT EXISTS '${WP_USER_LOGIN}'@'localhost' IDENTIFIED BY '${WP_USER_PASSWORD}';"
 
-mysql -e "GRANT ALL PRIVILEGES ON `${WP_TITLE}`.* TO `${WP_USER_LOGIN}`@'%' IDENTIFIED BY '${WP_USER_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${WP_TITLE}\`.* TO '${WP_USER_LOGIN}'@'%' IDENTIFIED BY '${WP_USER_PASSWORD}';"
 
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 mysql -e "FLUSH PRIVILEGES;"
+
 
 fi
 
