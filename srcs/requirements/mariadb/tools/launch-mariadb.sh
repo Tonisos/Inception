82c4eb980a483b6c0363193189e967ddcd2a9995
42MacAntoine <amontalb@student.42lyon.fr>
2023-11-28 12:22:43 +0100

    .

--- srcs/requirements/mariadb/tools/launch-mariadb.sh
+++ srcs/requirements/mariadb/tools/launch-mariadb.sh
@@ -6,32 +6,32 @@ set -e
 service mysql start
 
 # Safe install of MySQL
-# if [ ! -d "/var/lib/mysql/$WP_TITLE" ]
+if [ ! -d "/var/lib/mysql/$WP_TITLE" ]
 
-# then
+then
 
-# mysql_secure_installation << EOF
+mysql_secure_installation << EOF
 
-# n
-# y
-# y
-# y
-# y
-# EOF
+n
+y
+y
+y
+y
+EOF
 
-# Generate the database and the user with privilege for WordPress
+Generate the database and the user with privilege for WordPress
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${WP_TITLE}\`;"
+mysql -e "CREATE DATABASE IF NOT EXISTS ${WP_TITLE};"
 
-mysql -e "CREATE USER IF NOT EXISTS \`${WP_USER_LOGIN}\`@'localhost' IDENTIFIED BY '${WP_USER_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS '${WP_USER_LOGIN}'@'localhost' IDENTIFIED BY '${WP_USER_PASSWORD}';"
 
-mysql -e "GRANT ALL PRIVILEGES ON \`${WP_TITLE}\`.* TO \`${WP_USER_LOGIN}\`@'%' IDENTIFIED BY '${WP_USER_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON `${WP_TITLE}`.* TO `${WP_USER_LOGIN}`@'%' IDENTIFIED BY '${WP_USER_PASSWORD}';"
 
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 mysql -e "FLUSH PRIVILEGES;"
 
-# fi
+fi
 
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
